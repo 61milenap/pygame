f8fb17d9d3001f1a3d51de058273cc7d3afe934c
61milenap <61milenap8678@gmail.com>
2022-01-06 17:39:00 +0500

    1

--- import pygame.py
+++ import pygame.py
@@ -3,21 +3,11 @@ import sys
 import pygame
 
 pygame.init()
-pygame.key.set_repeat(200, 70)
-
 FPS = 50
-WIDTH = 600
-HEIGHT = 400
-STEP = 10
-
+WIDTH = 800
+HEIGHT = 700
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-
-player = None
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -36,52 +26,23 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
-
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
-
-
 def terminate():
     pygame.quit()
     sys.exit()
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
+    intro_text = ["ТЕТРИС", "",
                   "Правила игры",
                   "Если в правилах несколько строк,",
                   "приходится выводить их построчно"]
 
     fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
+    font = pygame.font.Font(None, 60)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -99,52 +60,36 @@ def start_screen():
         pygame.display.flip()
         clock.tick(FPS)
 
-
-tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
-player_image = load_image('mar.png')
-
-tile_width = tile_height = 50
 start_screen()
 
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+class Board:
+        def __init__(self, width, height):
+            self.width = width
+            self.height = height
+            self.board = [[0] * width for _ in range(height)]
+            self.left = 10
+            self.top = 10
+            self.cell_size = 30
+
+        def set_view(self, left, top, cell_size):
+            self.left = left
+            self.top = top
+            self.cell_size = cell_size
+
+        def render(self, screen):
+            for x in range(self.width):
+                for y in range(self.height):
+                    pygame.draw.rect(screen, (255, 255, 255), (
+                        x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size),
+                                     1)
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
-player, level_x, level_y = generate_level(load_level('map.txt'))
-
+board = Board(10, 20)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if pygame.key.get_pressed()[pygame.K_LEFT]:
-                player.rect.x -= STEP
-            if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                player.rect.x += STEP
-            if pygame.key.get_pressed()[pygame.K_DOWN]:
-                player.rect.y += STEP
-            if pygame.key.get_pressed()[pygame.K_UP]:
-                player.rect.y -= STEP
-
-    screen.fill(pygame.Color('white'))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-
-    pygame.display.flip()
-
-    clock.tick(FPS)
-
-pygame.quit()
+    screen.fill((0, 0, 0))
+    board.render(screen)
+    pygame.display.flip() 
