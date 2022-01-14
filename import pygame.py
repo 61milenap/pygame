bb5afb54b4d4c502be7d67da85b28a053fb7caa0
61milenap <61milenap8678@gmail.com>
2022-01-14 14:12:12 +0500

    123

--- import pygame.py
+++ import pygame.py
@@ -7,23 +7,24 @@ pygame.display.set_caption("Тетрис")
 pygame.init()
 FPS = 25
 
-#размеры самого экрана
+# размеры самого экрана
 WIDTH = 400
 HEIGHT = 500
 size = (WIDTH, HEIGHT)
-#размеры поля
+# размеры поля
 p_height = 400
 p_width = 200
-#крайние левые точки
+# крайние левые точки
 top_left_x = (WIDTH - p_width) // 2
 top_left_y = HEIGHT - p_height
-#цвета блоков
+# цвета блоков
 colors_block = [(255, 0, 0), (255, 125, 0), (255, 255, 0),
-          (0, 255, 0), (175, 214, 255), (0, 0, 255),
-          (170, 0, 255)]
+                (0, 255, 0), (175, 214, 255), (0, 0, 255),
+                (170, 0, 255)]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -42,9 +43,11 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+
 def terminate():
     pygame.quit()
     sys.exit()
+
 
 def start_screen():
     pygame.font.init()
@@ -52,10 +55,12 @@ def start_screen():
     screen.blit(fon, (0, 0))
     font = pygame.font.SysFont('georgia', 60)
     name1 = font.render('ТЕТРИС', 1, ('white'))
-    screen.blit(name1, (top_left_x + p_width / 2 - (name1.get_width() / 2), 20))
+    screen.blit(name1, (top_left_x + p_width /
+                2 - (name1.get_width() / 2), 20))
     font3 = pygame.font.SysFont('cambria', 80)
     name2 = font3.render('Играть', 2, ('yellow'))
-    screen.blit(name2, (top_left_x + p_width / 2 - (name2.get_width() / 2), HEIGHT // 2 - 50))
+    screen.blit(name2, (top_left_x + p_width / 2 -
+                (name2.get_width() / 2), HEIGHT // 2 - 50))
     intro_text = ["Цель игры:",
                   "заполнить как можно больше горизонтальных линий",
                   "на игровом поле, размещая опускающиеся фигуры",
@@ -75,13 +80,24 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or \
-                event.type == pygame.MOUSEBUTTONDOWN:
-                return # начинаем игру
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
 
+
 start_screen()
 
+pygame.mixer.music.load("sounds/tetris.mp3")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    clock.tick(FPS)
+    
+
+pygame.mixer.music.play(-1)
 running = True
 game = Tetris(20, 10)
 counter = 0
@@ -118,15 +134,16 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 pressing_down = False
-        
+
         if event.type == pygame.QUIT:
-                terminate()
+            terminate()
 
     screen.fill('black')
 
     for i in range(game.height):
         for j in range(game.width):
-            pygame.draw.rect(screen, 'white', [game.x + game.cell_size * j, game.y + game.cell_size * i, game.cell_size, game.cell_size], 1)
+            pygame.draw.rect(screen, 'white', [
+                             game.x + game.cell_size * j, game.y + game.cell_size * i, game.cell_size, game.cell_size], 1)
             if game.board[i][j] > 0:
                 pygame.draw.rect(screen, colors_block[game.board[i][j]],
                                  [game.x + game.cell_size * j + 1, game.y + game.cell_size * i + 1, game.cell_size - 2, game.cell_size - 1])
@@ -138,16 +155,17 @@ while running:
                 if p in game.block.image():
                     pygame.draw.rect(screen, colors_block[game.block.color],
                                      [game.x + game.cell_size * (j + game.block.x) + 1,
-                                      game.y + game.cell_size * (i + game.block.y) + 1,
+                                      game.y + game.cell_size *
+                                      (i + game.block.y) + 1,
                                       game.cell_size - 2, game.cell_size - 2])
 
     font = pygame.font.SysFont('cambria', 25, True, False)
     font1 = pygame.font.SysFont('cambria', 35, True, False)
 
     text = font.render("СЧЁТ " + str(game.score), True, 'red')
-    
+
     gameover1 = font1.render("Press ESC", True, 'red')
-    gameover= font1.render("ИГРА ОКОНЧЕНА", True, 'red')
+    gameover = font1.render("ИГРА ОКОНЧЕНА", True, 'red')
 
     screen.blit(text, [0, 0])
     if game.poss == "ИГРА ОКОНЧЕНА":
